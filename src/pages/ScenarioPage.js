98c10cc0a4d061be0de26ac9bcd20a90e3d1dc8b
kbugz <kbugusky@gmail.com>
2024-02-23 13:40:05 -0600

    added scenario page initial logic

--- src/pages/ScenarioPage.js
+++ src/pages/ScenarioPage.js
@@ -2,16 +2,54 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 export default function ScenarioPage() {
+    const [scenarioName, setScenarioName] = useState(''); // sets scenario name
+    const [numPlayers, setNumPlayers] = useState(''); // Ask how many players
+    const [displayForm, setDisplayForm] = useState(false); // Initial questions prior. 
+    const handleInputChange = (event) => { setScenarioName(event.target.value) }; 
+    const handleSubmit = (event) => { 
+        event.preventDefault(); // prevent page refresh
+        };
+    const handleSelectChange = (event) => {
+        setNumPlayers(event.target.value);
+        setDisplayForm(true);
+        };
 
     return (
-    <div> 
-        <p>Hello! I am working.</p>
+    <div>
+      {!displayForm ? (
+        <form onSubmit={handleSubmit}>
+          <ScenarioLabel>Scenario Name:
+            <input type="text" value={scenarioName} onChange={handleInputChange} />
+          </ScenarioLabel>
+
+          <label>
+            Select number of players:
+            <select value={numPlayers} onChange={handleSelectChange}>
+              <option value="">Select</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </label>
+
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <div>
+          <h1>{scenarioName}</h1>
+        </div>
+      )}
     </div>
-  );
+    );
 }
 
 const moo = styled.div``
 
-const moo2 = styled.div``
+const ScenarioLabel = styled.label`
+color: white;
+font-size:24px;
+`
 
 const moo3 = styled.div``
